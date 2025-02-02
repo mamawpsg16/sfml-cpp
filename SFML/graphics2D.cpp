aa5a95fd953af60c7b340570f401f3bd0b240f0a
Kevin <kevinmensah114@gmail.com>
2025-02-02 21:23:29 +0800

    update

--- SFML/graphics2D.cpp
+++ SFML/graphics2D.cpp
@@ -1,5 +1,16 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <math.h>
+
+sf::Vector2f NormalizedVector(const sf::Vector2f& vector) {
+	float m = std::sqrt((vector.x * vector.x) + (vector.y * vector.y));
+
+	sf::Vector2f normalizedVector;
+	normalizedVector.x = vector.x / m;
+	normalizedVector.y = vector.y / m;
+
+	return normalizedVector;
+}
 
 int main()
 {
@@ -7,28 +18,48 @@ int main()
 	sf::ContextSettings settings;
 	settings.antiAliasingLevel = 8;
 
-	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML shapes", sf::Style::Default, sf::State::Windowed, settings);
+	sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "SFML shapes", sf::Style::Default, sf::State::Windowed, settings);
 
 	//std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
 
-	sf::Texture skeletonTexture;
-	if (!skeletonTexture.loadFromFile("resources/player/textures/skeleton_spritesheet.png"))
-		//if (!skeletonTexture.loadFromFile("../resources/player/textures/skeleton.png"))
+	std::vector<sf::RectangleShape> bullets;
+	float bulletSpeed = 1.0f;
+
+	sf::Texture playerTexture;
+	if (!playerTexture.loadFromFile("resources/player/textures/skeleton_spritesheet.png"))
 	{
 		std::cerr << "Error: Could not load texture!" << std::endl;
-		int age;
-		std::cin >> age;
 		return -1;
 	}
 
 	// ✅ Declare and initialize the sprite after texture is loaded
-	sf::Sprite skeletonSprite(skeletonTexture);
-
+	sf::Sprite playerSprite(playerTexture);
 	// Set the texture rectangle (X, Y, WIDTH, HEIGHT)
 	int xIndex = 0;
 	int yIndex = 0;
-	skeletonSprite.setTextureRect(sf::IntRect({ xIndex * 64, yIndex * 64 }, { 64, 64 }));
-	skeletonSprite.scale(sf::Vector2f({ 2.0f, 2.0f }));
+	playerSprite.setTextureRect(sf::IntRect({ xIndex * 64, yIndex * 64 }, { 64, 64 }));
+	playerSprite.scale(sf::Vector2f({ 2.0f, 2.0f }));
+
+	
+
+	sf::Texture enemyTexture;
+	if (!enemyTexture.loadFromFile("resources/player/textures/skeleton_spritesheet.png"))
+	{
+		std::cerr << "Error: Could not load texture!" << std::endl;
+		return -1;
+	}
+	sf::Sprite enemySprite(enemyTexture);
+	// Set the texture rectangle (X, Y, WIDTH, HEIGHT)
+	int xEnemyIndex = 0;
+	int yEnemyIndex = 2;
+	enemySprite.setTextureRect(sf::IntRect({ xEnemyIndex * 64, yEnemyIndex * 64 }, { 64, 64 }));
+	enemySprite.scale(sf::Vector2f({ 2.0f, 2.0f }));
+	enemySprite.setPosition(sf::Vector2f(200.0, 250.0f));
+
+	//sf::Vector2f bulletDirection = enemySprite.getPosition() - bullets.getPosition();
+	//bulletDirection = NormalizedVector(bulletDirection);
+	
+	
 
 
 
@@ -84,6 +115,7 @@ int main()
 	line.setPosition(sf::Vector2f(100, 0));
 
 
+
 	while (window.isOpen())
 	{
 		// check all the window's events that were triggered since the last iteration of the loop
@@ -95,35 +127,49 @@ int main()
 			}
 			/*else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
 				if (keyPressed->scancode == sf::Keyboard::Scancode::D) {
-					sf::Vector2f position = skeletonSprite.getPosition();
+					sf::Vector2f position = playerSprite.getPosition();
 					std::cout << "X =" << position.x << "Y =" << position.y << '\n';
-					skeletonSprite.setPosition(position + sf::Vector2f(1.0f, 0.0f));
+					playerSprite.setPosition(position + sf::Vector2f(1.0f, 0.0f));
 				}
 			}*/
 		}
-		sf::Vector2f position = skeletonSprite.getPosition();
+		sf::Vector2f position = playerSprite.getPosition();
+		//bullet.setPosition(bullet.getPosition() + bulletDirection * bulletSpeed);
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
 			std::cout << "Moving Right\n"; // Moves as long as key is held
 			std::cout << "X =" << position.x << "Y =" << position.y << '\n';
-			skeletonSprite.setPosition(position + sf::Vector2f(1.0f, 0.0f));
+			playerSprite.setPosition(position + sf::Vector2f(1.0f, 0.0f));
 		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
 			std::cout << "Moving Left\n"; // Moves as long as key is held
 			std::cout << "X =" << position.x << "Y =" << position.y << '\n';
-			skeletonSprite.setPosition(position - sf::Vector2f(1.0f, 0.0f));
+			playerSprite.setPosition(position - sf::Vector2f(1.0f, 0.0f));
 		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
 			std::cout << "Moving Up\n"; // Moves as long as key is held
 			std::cout << "X =" << position.x << "Y =" << position.y << '\n';
-			skeletonSprite.setPosition(position - sf::Vector2f(0.0f, 1.0f));
+			playerSprite.setPosition(position - sf::Vector2f(0.0f, 1.0f));
 		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
 			std::cout << "Moving Down\n"; // Moves as long as key is held
 			std::cout << "X =" << position.x << "Y =" << position.y << '\n';
-			skeletonSprite.setPosition(position + sf::Vector2f(0.0f, 1.0f));
+			playerSprite.setPosition(position + sf::Vector2f(0.0f, 1.0f));
+		}
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+			bullets.emplace_back(sf::RectangleShape(sf::Vector2f(20, 10)));
+			int i = bullets.size() - 1;
+			bullets[i].setPosition(playerSprite.getPosition());
+
 		}
 
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			sf::Vector2f bulletDirection = enemySprite.getPosition() - bullets[i].getPosition();
+			bulletDirection = NormalizedVector(bulletDirection);
+
+			bullets[i].setPosition(bullets[i].getPosition() + bulletDirection * bulletSpeed);
+		}
 
 		//// clear the window with black color
 		//window.clear(sf::Color::Black);
@@ -139,7 +185,12 @@ int main()
 			window.draw(rectangle);
 			window.draw(convex);
 			window.draw(line);*/
-		window.draw(skeletonSprite);
+		window.draw(enemySprite);
+		window.draw(playerSprite);
+		for (size_t i = 0; i < bullets.size(); i++)
+		{
+			window.draw(bullets[i]);
+		}
 		//// end the current frame
 		window.display();
 	}
